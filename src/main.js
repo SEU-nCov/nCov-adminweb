a4d717b7811efc0ee70465251dd1b34affb8f6f2
zouyingcao <57442064+zouyingcao@users.noreply.github.com>
2022-11-29 12:46:57 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
+import echarts from "echarts";
+import Viewer from 'v-viewer'
+import 'viewerjs/dist/viewer.css'
 import 'element-ui/lib/theme-chalk/index.css';
+
 
 import router from '../router'
 import store from '../store'
+import admin from '../store/admin.js'
 //import hotel from '../store/hotel.js'
 import http from 'axios'
 import global from './global/global.js'
@@ -13,31 +18,64 @@ import global from './global/global.js'
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+Vue.use(Viewer)
 
-Vue.prototype.$http=http;
-Vue.prototype.$global=global
-//Vue.prototype.$hotel=hotel
+Vue.prototype.$http = http;
+Vue.prototype.$global = global
+Vue.prototype.$echarts = require("echarts")
+Vue.prototype.$admin = admin
+// Vue.prototype.$echarts = echarts;
 // Vue.prototype.$confirm=MessageBox.confirm;
 
-router.beforeEach((to,from,next)=>{
-  
-  store.commit('getToken')
-  const token =store.state.user.token
-  if(!token&&to.name!=='login'){
-    next({name:'login'})
-  }else if(token && to.name==='login'){
-    next({name:'home'})
-  }
-  else{
-    next()
-  }
-  // store.commit('clearToken')
+Viewer.setDefaults({
+	Options: {
+		'inline': true,
+		'button': true,
+		'navbar': true,
+		'title': true,
+		'toolbar': true,
+		'tooltip': true,
+		'movable': true,
+		'zoomable': true,
+		'rotatable': true,
+		'scalable': true,
+		'transition': true,
+		'fullscreen': true,
+		'keyboard': true,
+		'url': 'data-source'
+	}
+})
+
+router.beforeEach((to, from, next) => {
+
+	store.commit('getToken')
+	const token = store.state.user.token
+	if (!token && to.name !== 'login') {
+		next({
+			name: 'login'
+		})
+	} else if (token && to.name === 'login') {
+		next({
+			name: 'home'
+		})
+	} else {
+		next()
+	}
+	// store.commit('clearToken')
+})
+router.afterEach((to, from) => {
+	// if(from.path != '/' && to.path != '/' && to.path != '/admin'){
+	//     window.location.reload();
+	// }
+	// if(from.path != '/' && to.path == '/nCov_data'){
+	//      window.location.reload();
+	// }
 })
 new Vue({
-  store,
-  router,
-  render: h => h(App),
-  created(){
-    this.$store.commit('addMenu',router)
-  }
+	store,
+	router,
+	render: h => h(App),
+	created() {
+		this.$store.commit('addMenu', router)
+	}
 }).$mount('#app')
